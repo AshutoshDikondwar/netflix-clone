09dc0a18995afeb4716dc4dc637b80b99e6e0b95
Ashutosh <iamashutoshdikondwar@gmail.com>
2023-07-24 01:49:04 +0530

    netflix

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,7 +18,7 @@ const HomeScreen = () => {
             <Row title='NETFLIX ORIGINALS' fetchURL={requests.fetchNetflixOriginals}
                 isLargeRow
             />
-            <Row title='Trending Now' fetchURL={requests.fetchTopRated} />
+            {/* <Row title='Trending Now' fetchURL={requests.fetchTopRated} /> */}
             <Row title='Action Movies' fetchURL={requests.fetchActionMovies} />
             <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
             <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
